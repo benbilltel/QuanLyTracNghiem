a474b975f2f557af94ecc68ef1a4040bb07f9579
Ben <benbilldev@gmail.com>
2023-12-04 12:11:30 +0700

    db init

---
+++ QLTracNghiem/Migrations/202312040410433_DbInit.cs
@@ -0,0 +1,27 @@
+﻿namespace QLTracNghiem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DbInit : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.UserAdmin",
+                c => new
+                    {
+                        Ma = c.Int(nullable: false, identity: true),
+                        TaiKhoan = c.String(nullable: false, maxLength: 20),
+                        MatKhau = c.String(nullable: false, maxLength: 20),
+                    })
+                .PrimaryKey(t => t.Ma);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.UserAdmin");
+        }
+    }
+}
---
+++ QLTracNghiem/Models/QLTracNghiemContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLTracNghiem.Models
+{
+    public class QLTracNghiemContext : DbContext
+    {
+        public QLTracNghiemContext() : base("Data Source=DESKTOP-LTSC8K9\\SQLEXPRESS;Initial Catalog=QLTracNghiem;Integrated Security=True")
+        {
+
+        }
+        public virtual DbSet<UserAdmin> UserAdmins { get; set; }
+    }
+}
